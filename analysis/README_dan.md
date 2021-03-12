8a18e9c114a5428596615ffe0e8aa7e1ebd27c83
lunchbags <daniely3885@gmail.com>
2021-03-12 15:46:29 -0700

    task 3 completed with proper process and load python file and test notebook to verify it works

---
+++ analysis/README_dan.md
@@ -0,0 +1,10 @@
+## Task 1
+
+1. this is daniel's readme for task 1
+2. the other readme file is edward's
+
+**the scripts contains 2 .ipynb files for testing the load function for both myself and edward**
+*the processed_functions contains the loading function code copied and pasted as raw text*
+
+> ***inside of the clean_titles.ipynb is where most of the coding for task 3 is, the very bottom is simply testing the code for loading to guarantee it works***
+
--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,6 +1,51 @@
 import pandas as pd
 import numpy as np
 
-from data import raw
-df = project_functions.load_and_process('TwitchDataSet.csv')
-df
+def load_and_process(url_or_path_to_csv_file):
+
+    df4 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .rename(columns={"screen name": "Stream Name"})
+        .rename(columns={"top count":"Ranking"})
+        .rename(columns={"watch time":"Watch Time (Hours)"})
+        .rename(columns={"stream time":"Stream Time (Hours)"})
+        .rename(columns={"peak viewers":"Viewers (Peak)"})
+        .rename(columns={"average viewers":"Viewers (Average)"})
+        .rename(columns={"followers":"Followers (Total)"})
+        .rename(columns={"followers gained":"Followers (Gained)"})
+        .rename(columns={"views gained":"Viewers (Gained)"})
+        .rename(columns={"partnered":"Partnered"})
+        .rename(columns={"mature":"Audience Type (Mature)"})
+        .rename(columns={"language":"Language"})
+        .rename(columns={"first category":"Primary Game"})
+        .rename(columns={"second category":"Secondary Game"})
+        .rename(columns={"third category":"Tertiary Game"})
+        .drop("profile picture", axis = 1)
+        .drop("completa name", axis = 1)
+      )
+
+    df5 = (
+    df4.head(20).style.set_table_styles(
+        [{'selector': 'th',
+        'props': [('background', '#6441A4'),
+             ('color', 'white'),
+             ('font-family', 'verdana')]},
+     
+        {'selector': 'td',
+        'props': [('font-family', 'verdana')]},
+     
+         {'selector': 'tr:nth-of-type(odd)',
+         'props': [('background', '#DCDCDC')]},
+     
+         {'selector': 'tr:nth-of-type(even)',
+         'props': [('background', 'white')]},
+     
+         {'selector': 'tr:hover',
+         'props': [('background-color', '#F5DEF9')]}
+        ]
+    
+    ).hide_index()
+    
+    )
+
+    return df5
